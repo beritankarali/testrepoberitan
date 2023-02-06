39c19f136cab32cb563b42197b1a130e2c043004
NUCAL <nusec_dev_beritanprodtest@yopmail.com>
2023-02-06 13:59:30 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

--- src/components/pages/Screen1.vue
+++ src/components/pages/Screen1.vue
@@ -1,24 +1,24 @@
 <template>
-  <NucGridPage v-bind:gap="{'row':0,'col':0}" v-bind:designMode="false" overflow="auto" v-bind:rows="[1]"
-    v-bind:columns="[1]"
-    v-bind:schema="{'r0-c0':{'row':0,'col':0,'rowSpan':1,'colSpan':1,'showIn':['xs','sm','md','lg']}}"
-    v-bind:minNsLimit="816"
-    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingBottom':'0px','paddingLeft':'0px','paddingRight':'0px'}">
+  <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
+    v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="816"
+    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
     <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
-      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left" v-bind:row="0"
-      v-bind:col="0" v-bind:rowSpan="1" v-bind:colSpan="1" v-bind:marginLeft="0" v-bind:marginRight="0"
-      v-bind:marginTop="0" v-bind:marginBottom="0" id="c59cb09a-fa76-472a-8323-fbce82dcb9cc"
+      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
+      id="c59cb09a-fa76-472a-8323-fbce82dcb9cc" v-bind:col="0" v-bind:row="0" v-bind:colSpan="1" v-bind:rowSpan="1"
+      v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
       v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
       slot="r0-c0" area="r0-c0">
-      <WAccordionCollapse tag="div" v-bind:pill="false" size="md" text="Accordion 1" color="" v-bind:isNav="false"
+      <WAccordionCollapse tag="div" top="3.7654728084415585%" left="1.874542236328125%" v-bind:pill="false" size="md"
+        text="Accordion 1" color="" v-bind:isNav="false" v-bind:snapX="false" v-bind:snapY="false"
         theme="theme--light01" v-bind:active="false" v-bind:appear="false"
         content="I start opened because visible is true 
 
  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore VHS."
         v-bind:squared="false" variant="info" v-bind:visible="true" content2="I am collapsible second content!"
-        accordion="" v-bind:isEnabled="true" background="" borderColor="rgb(210, 218, 236)" v-bind:hasAccordion="true"
-        ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText="" textAccordion="Accordion 2"
-        verticalWidth="60px"
+        position="absolute" accordion="" v-bind:isEnabled="true" background="" borderColor="rgb(210, 218, 236)"
+        v-bind:hasAccordion="true" ncTypography="body1" v-bind:resetTrigger="false" ncTooltipText=""
+        textAccordion="Accordion 2" verticalWidth="60px"
         v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'content','displayName':'Content'},{'type':'string','propName':'text','displayName':'Text'}]}"
         verticalHeight="60px" v-bind:appearAccordion="false" horizontalWidth="72.4479px"
         contentAccordion="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore VHS."
@@ -26,20 +26,19 @@
         v-bind:visibleAccordion="false" ncClickBorderColor="#4285f4" ncDisableFontColor="#FFFFFF"
         ncFocusBorderColor="#4285f4" ncHoverBorderColor="#4285f4" v-bind:controlOfToggleCard="false"
         ncDisableBorderColor="transparent" ncClickBackgroundColor="#4285f4" ncFocusBackgroundColor="#4285f4"
-        ncHoverBackgroundColor="#4285f4" ncDisableBackgroundColor="#969696" position="absolute"
-        top="3.7654728084415585%" left="1.874542236328125%" v-bind:snapX="false" v-bind:snapY="false"
-        id="2f1a700b-2d60-4ff6-a8ff-f2f95bfd89b5"
-        v-bind:style="{'width':'573.6479999999999px','height':'','fontSize':'10.545px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'','lineHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','textDecoration':'none','borderLeftWidth':'0px','borderRightWidth':'0px','horizontalHeight':'26.3868941px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
-      <LottieChristmasCelebration src="https://assets9.lottiefiles.com/packages/lf20_8o0tj3yf.json" v-bind:loop="false"
-        v-bind:count="0" v-bind:hover="false" v-bind:speed="1" v-bind:autoplay="true" v-bind:direction="1"
-        v-bind:openCount="false" background="transparent" ncTooltipText="" position="absolute" top="5.9735186688311686%"
-        left="41.9525146484375%" v-bind:snapX="false" v-bind:snapY="false" id="f456145b-d1b4-4ff0-b3ea-dd2c5dbd4580"
-        v-bind:style="{'width':'281.2px','height':'210.89999999999998px','maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#000000','borderStyle':'none','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':3}"/>
-      <LottieSantaSnowman src="https://assets9.lottiefiles.com/packages/lf20_rkl7WiNZbx.json" v-bind:loop="false"
-        v-bind:count="0" v-bind:hover="false" v-bind:speed="1" v-bind:autoplay="true" v-bind:direction="1"
-        v-bind:openCount="false" background="transparent" ncTooltipText="" position="absolute" top="37.79170048701299%"
-        left="3.749847412109375%" v-bind:snapX="false" v-bind:snapY="false" id="105825ef-9789-4246-93bc-e49ff21cc363"
-        v-bind:style="{'width':'98.41999999999999px','height':'112.47999999999999px','maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#000000','borderStyle':'none','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':4}"/>
+        ncHoverBackgroundColor="#4285f4" ncDisableBackgroundColor="#969696" id="2f1a700b-2d60-4ff6-a8ff-f2f95bfd89b5"
+        v-bind:style="{'width':'573.6479999999999px','height':'','zIndex':2,'fontSize':'10.545px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'','lineHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','textDecoration':'none','borderLeftWidth':'0px','borderRightWidth':'0px','horizontalHeight':'26.3868941px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+      <LottieChristmasCelebration src="https://assets9.lottiefiles.com/packages/lf20_8o0tj3yf.json"
+        top="5.9735186688311686%" left="41.9525146484375%" v-bind:loop="false" v-bind:count="0" v-bind:hover="false"
+        v-bind:snapX="false" v-bind:snapY="false" v-bind:speed="1" v-bind:autoplay="true" position="absolute"
+        v-bind:direction="1" v-bind:openCount="false" background="transparent" ncTooltipText=""
+        id="f456145b-d1b4-4ff0-b3ea-dd2c5dbd4580"
+        v-bind:style="{'width':'281.2px','height':'210.89999999999998px','zIndex':3,'maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#000000','borderStyle':'none','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+      <LottieSantaSnowman src="https://assets9.lottiefiles.com/packages/lf20_rkl7WiNZbx.json" top="37.79170048701299%"
+        left="3.749847412109375%" v-bind:loop="false" v-bind:count="0" v-bind:hover="false" v-bind:snapX="false"
+        v-bind:snapY="false" v-bind:speed="1" v-bind:autoplay="true" position="absolute" v-bind:direction="1"
+        v-bind:openCount="false" background="transparent" ncTooltipText="" id="105825ef-9789-4246-93bc-e49ff21cc363"
+        v-bind:style="{'width':'98.41999999999999px','height':'112.47999999999999px','zIndex':4,'maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#000000','borderStyle':'none','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
     </NucGridCell>
   </NucGridPage>
 </template>
